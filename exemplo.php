acd9609ce7d70096b5e2849570a18e0e32007fe8
moratomicro <moratomicro@yahoo.com.br>
2021-11-12 16:00:05 -0300

    Busca de Cep online

---
+++ exemplo.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use \Moratomicro\DigitalCep\Search;
+
+$busca = new Search;
+
+$resultado = $busca->getAddressFromZipcode('01001000');
+
+print_r($resultado);
